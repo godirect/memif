87b558f6144c465bf6d8ee206b50144f05316bdc
rtan <rizhengt@gmail.com>
2021-08-16 07:25:23 -0700

    ci/cd

--- memif-attach/main.go
+++ memif-attach/main.go
@@ -1,19 +1,3 @@
-// Copyright (c) 2020 Cisco and/or its affiliates.
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at:
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -51,9 +35,7 @@ type HelloMsg struct {
 const MEMIF_SECRET_SIZE = 24
 type MemifInterfaceMode int32
 const (
-	MEMIF_INTERFACE_MODE_ETHERNET MemifInterfaceMode = iota
-	MEMIF_INTERFACE_MODE_IP // the one to create memif
-	MEMIF_INTERFACE_MODE_PUNT_INJECT
+	MEMIF_INTERFACE_MODE_IP MemifInterfaceMode = iota // the one to create memif
 )
 type InitMsg struct {
 	Ack AckMsg
@@ -95,15 +77,15 @@ func main() {
 	<-vppErrCh
 }
 
-func createMemifSocket(ctx context.Context, conn api.Connection) (socketID uint32, socket_filename string, err error) {
+func createMemifSocket(ctx context.Context, conn api.Connection) (uint32, string, error) {
 	c := memif.NewServiceClient(conn)
 	MemifSocketFilenameAddDel := memif.MemifSocketFilenameAddDel{
 		IsAdd:          true,
 		SocketID:       2,
 		SocketFilename: "/var/run/vpp/memif11.sock",
 	}
-	_, memifAddDel_err := c.MemifSocketFilenameAddDel(ctx, &MemifSocketFilenameAddDel)
-	if memifAddDel_err != nil {
+	_, memifAddDelErr := c.MemifSocketFilenameAddDel(ctx, &MemifSocketFilenameAddDel)
+	if memifAddDelErr != nil {
 		log.Entry(ctx).Fatalln("ERROR: MemifSocketFilenameAddDel failed:", memifAddDel_err)
 	}
 
